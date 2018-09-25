95b46614738084b340ed1f47ce9850ff7d533a35
Daniel Chimento <danchimento@live.com>
2018-09-24 20:33:13 -0400

    Setup

---
+++ js/game.js
@@ -0,0 +1,10 @@
+export default class Game {
+
+    constructor(element) {
+        var typed = new Typed(element, {
+            strings: ["First sentence.", "Second sentence."],
+            typeSpeed: 30
+          });
+    }
+
+}
